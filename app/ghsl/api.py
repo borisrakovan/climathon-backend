7eac7db52b2a70485b1dc0ec2ba008ca34ae64db
Jan Macek <macek.jan.sk@gmail.com>
2021-10-24 16:37:48 +0200

    fix: handle error when coordinates out of map coordinates

--- app/ghsl/api.py
+++ app/ghsl/api.py
@@ -30,43 +30,43 @@ class GhslFactor(BaseFactor):
         return [json.loads(gdf.to_json())['features'][0]['geometry']]
 
     def get_index_layer(self, bounds: List[float], size: List[int]) -> np.ndarray:
-        with rasterio.open(self.TIF_PATH) as data:
+        try:
+            with rasterio.open(self.TIF_PATH) as data:
 
-            geo = gpd.GeoDataFrame({
-                'geometry': box(bounds[0], bounds[1], bounds[2], bounds[3])
-            }, index=[0], crs=from_epsg(4326))
+                geo = gpd.GeoDataFrame({
+                    'geometry': box(bounds[0], bounds[1], bounds[2], bounds[3])
+                }, index=[0], crs=from_epsg(4326))
 
-            geo_bounds = geo.to_crs(crs=data.crs.data)
+                geo_bounds = geo.to_crs(crs=data.crs.data)
 
-            data_croppped, _ = rasterio.mask.mask(
-                dataset=data,
-                shapes=geo_bounds.geometry,
-                crop=True,
-                pad=True,
-                all_touched=True
-            )
+                data_croppped, _ = rasterio.mask.mask(
+                    dataset=data,
+                    shapes=geo_bounds.geometry,
+                    crop=True,
+                    pad=True,
+                    all_touched=True
+                )
 
-            m = max(data_croppped[0].shape[0], data_croppped[0].shape[1])
-            y = np.linspace(0, 1.0 / m, data_croppped[0].shape[0])
-            x = np.linspace(0, 1.0 / m, data_croppped[0].shape[1])
-            interpolating_function = RegularGridInterpolator((y, x), data_croppped[0])
+                m = max(data_croppped[0].shape[0], data_croppped[0].shape[1])
+                y = np.linspace(0, 1.0 / m, data_croppped[0].shape[0])
+                x = np.linspace(0, 1.0 / m, data_croppped[0].shape[1])
+                interpolating_function = RegularGridInterpolator((y, x), data_croppped[0])
 
-            yv, xv = np.meshgrid(np.linspace(0, 1.0 / m, size[0]), np.linspace(0, 1.0 / m, size[1]))
+                yv, xv = np.meshgrid(np.linspace(0, 1.0 / m, size[0]), np.linspace(0, 1.0 / m, size[1]))
 
-            out_image = interpolating_function((xv, yv))
-            out_image[out_image < 0] = 0
-            out_image = minmax_normalize(out_image)
-            out_image *= 2
-            # out_image -= 1
-            # out_image *= -1
-            # mean = 0.5
-            out_image = [out_image > 1.2] * (2 - out_image) + [out_image <= 1.2] * out_image
-            out_image = minmax_normalize(out_image)
-            return out_image
+                out_image = interpolating_function((xv, yv))
+                out_image[out_image < 0] = 0
+                out_image = minmax_normalize(out_image) * 2
+                out_image = [out_image > 1.2] * (2 - out_image) + [out_image <= 1.2] * out_image
+                out_image = minmax_normalize(out_image)
+
+                return out_image
+        except Exception:
+            return np.zeros(shape=(1, size[0], size[1])) + 0.5
 
 
 if __name__ == '__main__':
     show(GhslFactor().get_index_layer(
-        bounds=[16.941540682073867, 48.08774893052057, 17.241277652978773, 48.247859693476855],
+        bounds=[16.941540682073867, 48.08774893052057, 87.241277652978773, -148.247859693476855],
         size=[1000, 1000])
     )
